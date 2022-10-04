ec003d0264b93a983f9fb5ec08e1e262dc9f86bc
Rodrigo Silva <rodrigowstudy@gmail.com>
2022-10-04 13:10:24 -0300

    Help

---
+++ d3Jam/Assets/Scripts/Help.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Help : MonoBehaviour
+{
+    GameObject help;
+    bool open;
+    void Start()
+    {
+        help = GameObject.Find("Help");
+        help.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.H))
+        {
+            help.SetActive(!help.activeSelf);
+        }
+    }
+
+    private void OnDestroy() {
+        help.SetActive(false);
+    }
+}
